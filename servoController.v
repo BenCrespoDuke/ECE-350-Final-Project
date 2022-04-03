c632056f4d672b3fab22cd5d1e3c1d157fce367b
Benjamin Crespo <bjc58@duke.edu>
2022-04-03 16:09:56 -0400

    added servo controller file

---
+++ servoController.v
@@ -0,0 +1,9 @@
+module servoController(clck,IR,DataA,DataB,Direction1,Direction2,Direction3,Direction4,Signals);
+
+input clck;
+input [31:0] DataA,DataB,IR;
+output [1:0] Direction1,Direction2,Direction3,Direction4;
+output [3:0] Signals;
+
+
+endmodule
