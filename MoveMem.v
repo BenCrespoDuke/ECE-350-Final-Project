4845c0212472988678b3461d53d8da066dd3c8b2
Benjamin Crespo <bjc58@duke.edu>
2022-04-08 18:04:56 -0400

    fixed bypassing speed isn error

--- MoveMem.v
+++ MoveMem.v
@@ -1,10 +1,11 @@
 module MoveMem(
-    IR_In, DirIn, SpeedIn, Shift,clck,reset
+    IR_In, DirIn, SpeedIn, Shift,clck,reset,dirOut
 );
+input [31:0] IR_In,DirIn;
+input Shift,reset,clck;
 
 
-
-register dir1(data_In, data_Out, clk,en,clr);
+register dir1(dirOut, data_Out, clck,en,clr);
 register speed1(data_In, data_Out, clk,en,clr);
 register dir1(data_In, data_Out, clk,en,clr);
 register speed1(data_In, data_Out, clk,en,clr);
